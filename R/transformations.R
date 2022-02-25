aeb642c43d54e3eabec5ec09fb6169af9aa37b52
Bryan Quach <bryancquach@gmail.com>
2022-02-24 23:16:43 -0500

    added functions to do beta and m-value conversions

--- R/transformations.R
+++ R/transformations.R
@@ -1,4 +1,4 @@
-# Functions for applying transformations to data vectors.
+# Functions for applying transformations to data vectors or matrices.
 # Author: Bryan Quach (bryancquach@gmail.com)
 
 #' Rank inverse-normal transform
@@ -6,7 +6,7 @@
 #' Apply the rank inverse-normal transform (RINT) to a vector.
 #'
 #' @param data A numeric vector.
-#' @return A numeric vector.
+#' @return A numeric vector of normalized values.
 #' @export
 rint <- function(data) {
   if (!is.vector(data)) {
@@ -17,4 +17,28 @@ rint <- function(data) {
   scaled_rank <- rankings / (max(rankings, na.rm = T) + 0.5)
   rint_values <- qnorm(scaled_rank)
   return(rint_values)
+}
+
+#' DNA methylation M-values
+#'
+#' Apply M-value transform to DNA methylation (DNAm) array beta intensity values.
+#'
+#' @param data A numeric vector or matrix of DNAm array beta intensity values.
+#' @return A numeric vector or matrix of M-values.
+#' @export
+beta_to_mvalue <- function(data) {
+  mvalues <- log2(data / (1 - data))
+  return(mvalues)
+}
+
+#' DNA methylation beta values
+#'
+#' Convert DNA methylation (DNAm) array M-values to beta intensity values.
+#'
+#' @param data A numeric vector or matrix of DNAm array M-values.
+#' @return A numeric vector or matrix of beta intensity values.
+#' @export
+mvalue_to_beta <- function(data) {
+  betas <- (2 ^ data) / ((2 ^ data) + 1)
+  return(betas)
 }
